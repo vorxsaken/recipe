8c6a89eb2776e33899225e11b82badb728495475
vorxsaken <jhonyevans6326@gmail.com>
2023-04-07 04:52:56 +0700

    feat: add recipe list component to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Layout from "@/components/Layout"
+import Hero from "@/components/Landing/Hero"
+import RecipeList from "@/components/Landing/RecipeList"
 
 export default function Home() {
   return (
     <>
       <Layout>
-        
+        <Hero />
+        <RecipeList />
       </Layout>
     </>
   )
