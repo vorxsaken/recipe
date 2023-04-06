12c4571a055236dfb6a8cda309e35f51a49a993e
vorxsaken <jhonyevans6326@gmail.com>
2023-04-07 04:48:25 +0700

    feat: buttton size

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,9 +10,9 @@ export default function Button(props: button) {
     const { children, text } = props;
 
     return (
-        <div className={`w-28 h-10 rounded-lg flex justify-center items-center font-semibold
-        cursor-pointer ${text ? 'hover:text-red-600 text-slate-600 text-base md:text-[0.9rem]' :
-        'text-xs text-white bg-red-500 hover:bg-red-700 shadow-lg'}`}
+        <div className={` h-10 rounded-lg flex justify-center items-center font-semibold
+        cursor-pointer ${text ? 'w-auto hover:text-red-600 text-slate-600 text-base md:text-[0.9rem]' :
+        'w-28 text-xs text-white bg-red-500 hover:bg-red-700 shadow-lg'}`}
         {...props}>
             {children}
         </div>
