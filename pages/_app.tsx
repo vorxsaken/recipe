cf8c00c2b0e1de6af2bd4ce57352452d08b59f31
vorxsaken <jhonyevans6326@gmail.com>
2023-04-07 04:52:13 +0700

    feat: now global font is ubuntu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import FullScreenContentProvider from '@/Reducer/FullScreenContentReducer'
+import { Poppins } from 'next/font/google'
+const ubuntu = Poppins({ weight: '400', subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <FullScreenContentProvider>
+      <div className={ubuntu.className}>
+        <Component {...pageProps} />
+      </div>
+    </FullScreenContentProvider>
+  )
 }
