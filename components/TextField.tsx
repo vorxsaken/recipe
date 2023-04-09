4d96206f35956a9d3607cc6f0369fc4b5fb5f8e7
vorxsaken <jhonyevans6326@gmail.com>
2023-04-09 11:46:33 +0700

    feat: now it had option to be text field or text area

--- components/TextField.tsx
+++ components/TextField.tsx
@@ -1,12 +1,21 @@
 import { ReactNode } from 'react'
 
-interface textField { 
-    placeholder?: string, 
+interface textField {
+    placeholder?: string,
     icon?: ReactNode,
-    small?: boolean
+    small?: boolean,
+    textArea?: boolean,
+    width?: number,
+    height?: number
 }
 
-export default function TextField({ placeholder, icon, small}: textField) {
+export default function TextField({ placeholder, icon, small, textArea, width, height }: textField) {
+
+    const handleInput = (event: any) => {
+        event.currentTarget.style.height = 'auto';
+        event.currentTarget.style.height = `${event.currentTarget.scrollHeight}px`
+    }
+
     return (
         <div className="relative">
             {
@@ -16,8 +25,24 @@ export default function TextField({ placeholder, icon, small}: textField) {
                     </span>
                 )
             }
-            <input type="text" placeholder={placeholder} className={`${small ? 'w-20' : 'w-52'} h-8 outline-none border 
-            border-slate-800 rounded-2xl ${icon ? 'pl-9' : 'pl-4'} pr-4 text-xs placeholder:text-slate-800`} />
+            {
+                textArea ? (
+                    <>
+                        <textarea
+                            onInput={handleInput}
+                            className='w-full border-b border-slate-600 resize-none px-2 py-2 text-sm outline-none text-slate-700
+                            scrollbar-hide transition-all duration-700 ease-in-out placeholder:text-slate-500'
+                            cols={width}
+                            placeholder={placeholder}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <input type="text" placeholder={placeholder} className={`${small ? 'w-20' : 'w-52'} h-8 outline-none border 
+                       border-slate-800 rounded-2xl ${icon ? 'pl-9' : 'pl-4'} pr-4 text-xs placeholder:text-slate-800`} />
+                    </>
+                )
+            }
         </div>
     )
 }
