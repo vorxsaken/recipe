8dd3106d262c04201715e42a21a84829596ebb06
vorxsaken <jhonyevans6326@gmail.com>
2023-04-09 11:35:51 +0700

    chore: refactor recipe list structure

--- components/Landing/RecipeList.tsx
+++ components/Landing/RecipeList.tsx
@@ -1,17 +1,15 @@
+import SliderContainer from "../SliderContainer"
 import RecipeCard from "../RecipeCard"
-import SwipeContainer from "../SwipeContainer"
-import { Bungee } from 'next/font/google'
-const bungee = Bungee({ weight: '400', subsets: ['latin'] })
 
-export default function RecipeList() {
+export default function RecipeList({wrap}: {wrap?:boolean}) {
+    const array = [1,2,3,4,5,6,7,8]
+    const item = array.map(i => (
+        <RecipeCard />
+    ))
+
     return (
-        <div className="w-full flex flex-col justify-center items-center gap-8 px-20 py-5">
-            <div className={`w-full text-center text-3xl font-bold ${bungee.className}`}>
-                <span className="text-red-500">POPULAR</span> FOOD 🔥
-            </div>
-            <SwipeContainer>
-                
-            </SwipeContainer>
-        </div>
+        <>
+            <SliderContainer title="Hottest Food 🔥" items={item} wrap={wrap} />            
+        </>
     )
 }
