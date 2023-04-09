9bee9ba85fb8900227ef1b96ca943fa1a401ef2c
vorxsaken <jhonyevans6326@gmail.com>
2023-04-09 11:38:18 +0700

    style: change button border, background and background on hover

--- components/Button.tsx
+++ components/Button.tsx
@@ -11,8 +11,8 @@ export default function Button(props: button) {
 
     return (
         <div className={` h-10 rounded-lg flex justify-center items-center font-semibold
-        cursor-pointer ${text ? 'w-auto hover:text-red-600 text-slate-600 text-base md:text-[0.9rem]' :
-        'w-28 text-xs text-white bg-red-500 hover:bg-red-700 shadow-lg'}`}
+        cursor-pointer ${text ? 'w-28 hover:text-red-500 hover:bg-gray-50 text-slate-600 text-base md:text-[0.9rem] border border-slate-300' :
+        'w-28 text-xs text-white bg-red-500 hover:bg-red-600 shadow-lg'}`}
         {...props}>
             {children}
         </div>
