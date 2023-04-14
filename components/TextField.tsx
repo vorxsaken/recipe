cfc67e96bfad4772d9d9f74fd76f166e7089a6ec
vorxsaken <jhonyevans6326@gmail.com>
2023-04-14 15:19:19 +0700

    feat: now had no border option

--- components/TextField.tsx
+++ components/TextField.tsx
@@ -14,7 +14,7 @@ interface textField {
 
 export default function TextField(props: textField) {
 
-    const { placeholder, icon, small, textArea, width, height, medium, large } = props;
+    const { placeholder, icon, small, textArea, width, height, medium, large, borderLess } = props;
 
     const handleInput = (event: any) => {
         event.currentTarget.style.height = 'auto';
@@ -25,6 +25,7 @@ export default function TextField(props: textField) {
     const textFieldHeight = small ? 'h-8' : medium ? 'h-11' : large ? 'h-12' : 'h-8';
     const textFieldRounded = small ? 'rounded-2xl' : medium ? 'rounded-xl' : large ? 'rounded-lg' : 'rounded-2xl';
     const textFieldIcon = icon ? 'pl-9' : 'pl-4';
+    const textfieldBorder = borderLess ? 'border-none' : textArea ? 'border-b border-slate-600' : 'border border-slate-800';
 
     return (
         <div className="relative">
@@ -40,8 +41,8 @@ export default function TextField(props: textField) {
                     <>
                         <textarea
                             onInput={handleInput}
-                            className='w-full border-b border-slate-600 resize-none px-2 py-2 text-sm outline-none text-slate-700
-                            scrollbar-hide transition-all duration-700 ease-in-out placeholder:text-slate-500'
+                            className={`w-full resize-none px-2 py-2 text-sm outline-none text-slate-700
+                            scrollbar-hide transition-all duration-700 ease-in-out placeholder:text-slate-500 ${textfieldBorder}`}
                             cols={width}
                             placeholder={placeholder}
                             {...props}
@@ -50,8 +51,8 @@ export default function TextField(props: textField) {
                 ) : (
                     <>
                         <input type="text" placeholder={placeholder} className={`
-                        ${textFieldWidth} ${textFieldHeight} ${textFieldRounded} ${textFieldIcon} outline-none border 
-                        border-slate-800 pr-4 text-xs placeholder:text-slate-800`} {...props} />
+                        ${textFieldWidth} ${textFieldHeight} ${textFieldRounded} ${textFieldIcon} ${textfieldBorder}
+                        outline-none pr-4 text-xs placeholder:text-slate-800`} {...props} />
                     </>
                 )
             }
