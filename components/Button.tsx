dbcde5f7f242d04972547ec6e11f2df9ec2b2df1
vorxsaken <jhonyevans6326@gmail.com>
2023-04-14 15:18:00 +0700

    chore: remove shadow

--- components/Button.tsx
+++ components/Button.tsx
@@ -12,7 +12,7 @@ export default function Button(props: button) {
     return (
         <div className={` h-10 rounded-lg flex justify-center items-center font-semibold
         cursor-pointer ${text ? 'w-28 hover:text-red-500 hover:bg-gray-50 text-slate-600 text-base md:text-[0.9rem] border border-slate-300' :
-        'w-28 text-xs text-white bg-red-500 hover:bg-red-600 shadow-lg'}`}
+        'w-28 text-xs text-white bg-red-500 hover:bg-red-600'}`}
         {...props}>
             {children}
         </div>
