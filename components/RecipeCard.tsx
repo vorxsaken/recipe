9ac86a2ec040c0d31c13d83c25086f489ae6d08f
vorxsaken <jhonyevans6326@gmail.com>
2023-04-18 13:53:36 +0700

    chore: add calorie, like, rating in card appearance

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -1,6 +1,7 @@
 import Image, { StaticImageData } from 'next/image'
 import { BiTime } from 'react-icons/bi';
-import { AiFillHeart } from 'react-icons/ai'
+import { AiFillHeart, AiFillStar } from 'react-icons/ai'
+import { GiSpoon } from 'react-icons/gi'
 import Burger from '../assets/images/burger.jpg';
 
 interface recipeCard {
@@ -11,9 +12,9 @@ interface recipeCard {
   likeEvent?: () => void
 }
 
-export default function RecipeCard({ image, title, calorie, serving, likeEvent }: recipeCard ) {
+export default function RecipeCard({ image, title, calorie, serving, likeEvent }: recipeCard) {
   return (
-    <div className='w-40 h-40 md:w-72 md:h-64 flex flex-col flex-none justify-start items-center gap-2 border border-slate-300 
+    <div className='w-40 h-40 md:w-64 md:h-56 flex flex-col flex-none justify-start items-center gap-2 border border-slate-300 
     overflow-hidden rounded-lg'>
       <div className='w-full h-36 md:h-48 bg-blue-200 overflow-hidden relative group'>
         <Image src={Burger} alt='burger' className='object-cover pointer-events-none' />
@@ -24,11 +25,24 @@ export default function RecipeCard({ image, title, calorie, serving, likeEvent }
       </div>
       <div className='w-full flex flex-col text-md md:text-lg pl-4 text-slate-700 font-semibold'>
         <span>burger</span>
-        <span className='text-xs text-slate-400 font-thin'>220 cal</span>
+        <div className='flex flex-row gap-4'>
+          <div className='text-xs text-slate-400 font-thin flex flex-row gap-2'>
+            <GiSpoon className='text-red-400' />
+            <span>220 cal</span>
+          </div>
+          <div className='text-xs text-slate-400 font-thin flex flex-row gap-2'>
+            <AiFillHeart className='text-red-400' />
+            <span>10</span>
+          </div>
+          <div className='text-xs text-slate-400 font-thin flex flex-row gap-2'>
+            <AiFillStar className='text-red-400' />
+            <span>3</span>
+          </div>
+        </div>
       </div>
       <div className='w-full px-4 pb-2 flex justify-start items-center text-slate-800'>
-        <div className='flex justify-center items-center gap-2'>
-          <BiTime className='text-sm md:text-lg' />
+        <div className='flex justify-center items-center gap-1'>
+          <BiTime className='text-sm md:text-md' />
           <span className='text-[0.6rem] md:text-[0.7rem]'> 15 min serving</span>
         </div>
       </div>
