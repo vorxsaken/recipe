875876325cc9316a9ef074a309335a89930e28f2
vorxsaken <jhonyevans6326@gmail.com>
2023-04-18 13:51:19 +0700

    chore: add small size button

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,16 +3,17 @@ import { ReactNode } from 'react'
 interface button {
     children: ReactNode,
     text?: boolean,
+    small?: boolean,
     [key: string]: any
 }
 
 export default function Button(props: button) {
-    const { children, text } = props;
+    const { children, text, small } = props;
 
     return (
-        <div className={` h-10 rounded-lg flex justify-center items-center font-semibold
-        cursor-pointer ${text ? 'w-28 hover:text-red-500 hover:bg-gray-50 text-slate-600 text-base md:text-[0.9rem] border border-slate-300' :
-        'w-28 text-xs text-white bg-red-500 hover:bg-red-600'}`}
+        <div className={`rounded-md flex justify-center items-center font-semibold
+        cursor-pointer ${text ? 'hover:text-red-500 hover:bg-gray-50 text-slate-600 text-base md:text-[0.9rem] border border-slate-400' :
+        'text-xs text-white bg-red-500 hover:bg-red-600'} ${small ? 'w-24 h-8' : 'w-28 h-10'}`}
         {...props}>
             {children}
         </div>
