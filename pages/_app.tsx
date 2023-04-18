9a41b103f885925a8718af11ba226f1c914f6c72
vorxsaken <jhonyevans6326@gmail.com>
2023-04-18 13:56:20 +0700

    fix: remove fullscreen reducer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import FullScreenContentProvider from '@/Reducer/FullScreenContentReducer'
-import { Poppins } from 'next/font/google'
-const ubuntu = Poppins({ weight: '400', subsets: ['latin'] })
+import { Archivo } from 'next/font/google'
+const manrope = Archivo({ weight: '400', subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <FullScreenContentProvider>
-      <div className={ubuntu.className}>
-        <Component {...pageProps} />
-      </div>
-    </FullScreenContentProvider>
+    <div className={manrope.className}>
+      <Component {...pageProps} />
+    </div>
   )
 }
