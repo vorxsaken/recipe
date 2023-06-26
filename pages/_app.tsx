77c96ec6d311345f2fc004bec7061cb7e13d5bca
vorxsaken <jhonyevans6326@gmail.com>
2023-06-26 14:25:44 +0700

    feat: transition between page had loading progress now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,27 @@ import { Archivo } from 'next/font/google'
 import { SessionProvider } from 'next-auth/react'
 import store from '../store/index'
 import { Provider } from 'react-redux'
+import { useEffect } from 'react'
+import Router from "next/router"
+import NProgress from 'nprogress'
+import '../styles/nprogress.css'
 const manrope = Archivo({ weight: '400', subsets: ['latin'] })
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+
+  useEffect(() => {
+    NProgress.configure({showSpinner: false, trickleSpeed: 300});
+    
+    Router.events.on("routeChangeStart", (url) => {
+      NProgress.start();
+    })
+
+    Router.events.on("routeChangeComplete", (url) => {
+      NProgress.done(false);
+    })
+
+  }, [Router])
+
   return (
     <SessionProvider session={session}>
       <Provider store={store}>
