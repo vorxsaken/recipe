7631e853e2602ba646e1b560583a6ed98ed965e9
vorxsaken <jhonyevans6326@gmail.com>
2023-06-30 09:53:05 +0700

    fix: async error while converting image

--- components/CreateRecipe/SelectImage.tsx
+++ components/CreateRecipe/SelectImage.tsx
@@ -1,11 +1,16 @@
 import { BsImageFill } from 'react-icons/bs'
 import Image from 'next/image'
-import Burger from '../../assets/images/sbdkajbskjdakjsdk.jpg';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef} from 'react';
 import Compressor from 'compressorjs';
 
-export default function SelectImage({ onChange, value }: { onChange: (e: any) => void, value?: string }) {
-    const [image, setimage] = useState<Blob>()
+interface SelectImageUI {
+    onChange: (e: any) => void,
+    value?: string,
+    sm?: boolean,
+    circleRounded?: boolean
+}
+
+export default function SelectImage({ onChange, value, sm, circleRounded }: SelectImageUI) {
     const [imageUrl, setImageUrl] = useState(value || '')
     const ref = useRef(null)
 
@@ -15,25 +20,24 @@ export default function SelectImage({ onChange, value }: { onChange: (e: any) =>
             // small image for thumbnail and etc
             new Compressor(uncompressImage, {
                 quality: 0.8,
-                width: 200,
+                width: 300,
                 success(res) {
                     compressedImage = {
                         smallImage: res
                     }
-                }
-            })
-
-            // big image for image details and etc
-            new Compressor(uncompressImage, {
-                quality: 0.6,
-                width: 800,
-                success(res) {
-                    compressedImage = {
-                        ...compressedImage,
-                        bigImage: res
-                    }
-
-                    resolve(compressedImage)
+                    // big Image for real pic
+                    new Compressor(uncompressImage, {
+                        quality: 0.8,
+                        width: 600,
+                        success(res) {
+                            compressedImage = {
+                                ...compressedImage,
+                                bigImage: res
+                            }
+                            
+                            resolve(compressedImage)
+                        }
+                    })
                 }
             })
         })
@@ -45,33 +49,31 @@ export default function SelectImage({ onChange, value }: { onChange: (e: any) =>
     }
 
     const inputChange = async (e: any) => {
-        setimage(e.target.files[0])
-        const url = URL.createObjectURL(e.target.files[0])
+        const compressedImage = await compressImage(e.target.files[0]) as any;
+        const url = URL.createObjectURL(compressedImage.bigImage)
         setImageUrl(url);
-        const compressedImage = await compressImage(e.target.files[0]);
         onChange(compressedImage)
     }
 
-    useEffect(() => {
-        setimage(new Blob())
-    }, [])
+    const containerSize = sm ? 'w-[220px] h-[220px]' : 'w-[500px] h-[450px]';
+    const borderRadius = circleRounded ? 'rounded-full' : 'rounded-xl';
 
     return (
         <>
             <input onChange={(e) => inputChange(e)} ref={ref} type="file" className='hidden' />
             {
                 imageUrl ? (
-                    <div onClick={chooseImage} className='w-[500px] h-[450px] overflow-hidden rounded-xl cursor-pointer relative'>
+                    <div onClick={chooseImage} className={`${containerSize} ${borderRadius} overflow-hidden cursor-pointer relative`}>
                         <Image src={imageUrl} alt='recipe image preview' fill className='object-cover' />
                     </div>
                 ) : (
                     <div
                         onClick={chooseImage}
-                        className="w-[500px] h-[450px] bg-zinc-200 rounded-xl flex justify-center items-center cursor-pointer overflow-hidden"
+                        className={`${containerSize} ${borderRadius} bg-zinc-200 flex justify-center items-center cursor-pointer overflow-hidden`}
                     >
                         <div className='flex flex-col justify-center items-center gap-4'>
-                            <BsImageFill className='text-6xl text-zinc-400' />
-                            <p className='text-zinc-400'>
+                            <BsImageFill className={`${sm ? 'text-4xl' : 'text-6xl'} text-zinc-400`} />
+                            <p className={`text-zinc-400 ${sm && 'text-xs'}`}>
                                 Click this field to select image
                             </p>
                         </div>
