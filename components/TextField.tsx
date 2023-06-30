3ef7ff5a3c601abf256ad078ee8c6ed2069a2d0f
vorxsaken <jhonyevans6326@gmail.com>
2023-06-30 09:57:07 +0700

    chore: fix textarea width and add counter character feature

--- components/TextField.tsx
+++ components/TextField.tsx
@@ -1,8 +1,10 @@
-import { ReactNode, useEffect, forwardRef } from 'react'
+import { ReactNode, useEffect, forwardRef, useState } from 'react'
 
 interface textField {
     id?: any,
-    value?: string | number,
+    showCounter?: boolean,
+    initValue?: number,
+    autoGrow?: boolean,
     placeholder?: string,
     icon?: ReactNode,
     small?: boolean,
@@ -10,22 +12,24 @@ interface textField {
     large?: boolean,
     textArea?: boolean,
     width?: number,
-    height?: number,
     borderLess?: boolean,
     number?: boolean,
     className?: string,
+    bigPlaceholder?: boolean,
     [key: string]: any,
 }
 
 export default function TextField(props: textField) {
-
-    const { id, placeholder, icon, small, textArea, width, height, medium, large, borderLess, number, className, value } = props;
-    var defaultValue = value ?? '';
+    const { id, placeholder, icon, small, textArea, autoGrow, medium, large, borderLess, number, className, showCounter, initValue, bigPlaceholder } = props;
+    const [counter, setCounter] = useState(initValue?.toString().length || 0);
 
     const handleInput = (event: any) => {
-        if (textArea) {
+        const f = document.getElementById(id) as any;
+        setCounter(f.value.length);
+
+        if (autoGrow) {
             event.currentTarget.style.height = 'auto';
-            event.currentTarget.style.height = `${event.currentTarget.scrollHeight}px`
+            event.currentTarget.style.height = `${event.currentTarget.scrollHeight}px`;
         }
 
         if (number) {
@@ -35,21 +39,15 @@ export default function TextField(props: textField) {
         }
     }
 
-    const textFieldWidth = small ? 'w-20' : medium ? 'w-56' : large ? 'w-80' : 'w-full';
+    // const textFieldWidth = small ? 'w-20' : medium ? 'w-56' : large ? 'w-80' : 'w-full';
+    const Placeholder = bigPlaceholder ? 'placeholder:font-bold text-2xl font-black' : 'placeholder:text-slate-500 text-sm';
     const textFieldHeight = small ? 'h-8' : medium ? 'h-11' : large ? 'h-12' : 'h-8';
     const textFieldRounded = small ? 'rounded-md' : medium ? 'rounded-md' : large ? 'rounded-lg' : 'rounded-md';
     const textFieldIcon = icon ? 'pl-9' : 'pl-4';
     const textfieldBorder = borderLess ? 'border-none' : textArea ? 'border-b border-slate-500' : 'border border-slate-800';
 
-    useEffect(() => {
-        if (value) {
-            const textValue = document.getElementById(id) as any
-            textValue.value = defaultValue
-        }
-    }, [])
-    
     return (
-        <div className="relative">
+        <>
             {
                 icon && (
                     <span className="text-md text-slate-800 absolute top-[28%] left-3">
@@ -59,21 +57,28 @@ export default function TextField(props: textField) {
             }
             {
                 textArea ? (
-                    <>
+                    <div className='w-full'>
                         <textarea
                             id={id}
                             onInput={handleInput}
-                            className={`w-full resize-none px-2 py-2 text-sm outline-none text-slate-700
-                            scrollbar-hide transition-all duration-700 ease-in-out placeholder:text-slate-500 ${textfieldBorder} ${className}`}
-                            cols={width}
+                            className={`w-full ${!autoGrow && 'h-[80px]'} resize-none px-2 py-2 outline-none text-slate-700
+                            scrollbar-hide transition-all duration-700 ease-in-out ${Placeholder} ${textfieldBorder} ${className}`}
                             placeholder={placeholder}
+                            defaultValue={initValue || ''}
                             {...props}
                         />
-                    </>
+                        {
+                            showCounter && (
+                                <span className='text-sm text-gray-500'>
+                                    {counter}&nbsp; /150
+                                </span>
+                            )
+                        }
+                    </div>
                 ) : (
                     <>
                         <input id={id} type='text' placeholder={placeholder} className={`
-                        ${textFieldWidth} ${textFieldHeight} ${textFieldRounded} ${textFieldIcon} ${textfieldBorder}
+                        w-full ${textFieldHeight} ${textFieldRounded} ${textFieldIcon} ${textfieldBorder}
                         outline-none pr-4 text-xs placeholder:text-slate-800`}
                             onInput={handleInput}
                             {...props}
@@ -81,6 +86,6 @@ export default function TextField(props: textField) {
                     </>
                 )
             }
-        </div>
+        </>
     )
 }
