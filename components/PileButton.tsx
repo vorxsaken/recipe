e2c463898877a49a1ef75640fa7ceada6d889d3d
vorxsaken <jhonyevans6326@gmail.com>
2023-07-12 15:50:16 +0700

    fix: change how this button get the options and have the selected value

--- components/PileButton.tsx
+++ components/PileButton.tsx
@@ -1,24 +1,24 @@
-import { useState } from 'react'
-interface UIPileButton {
-    buttons: string[],
-    url: string[],
-    select?: string
-}
+import { ReactNode, useState } from 'react'
 
-export default function PileButton({ buttons, url, select }: UIPileButton) {
-    const [selected, setselected] = useState(select)
+export default function PileButton({children, value, defaultValue}: {children: ReactNode[], value: (sel: any) => void, defaultValue?: string}) {
+    const [selected, setselected] = useState(defaultValue || '')
+    const getselected = (val: any) => {
+        setselected(val);
+        value(val);
+    }
 
     return (
         <div className="h-8 md:h-9 border border-slate-400 flex flex-row justify-center items-center rounded-3xl overflow-hidden">
-            {buttons.map((butt, index) => (
-                <div key={index} className={`${index !== buttons.length - 1 ? 'border-r border-slate-300' : 'border-0'} 
-                ${butt === selected ? 'bg-zinc-100 text-red-500 ' : 'text-zinc-700'} 
-                hover:text-red-500 h-full w-full px-6 text-xs cursor-pointer flex justify-center items-center`}
-                onClick={() => setselected(butt)}
-                >
-                    {butt}
-                </div>
-            ))}
+            {
+                children?.map((child, index) => (
+                    <div key={index} className={`${index !== children.length - 1 ? 'border-r border-slate-300' : 'border-0'} 
+                    ${selected === (child as any).props.children ? 'bg-zinc-100 text-red-500 ' : 'text-zinc-700'} 
+                    hover:text-red-500 h-full w-full px-6 text-xs cursor-pointer flex justify-center items-center`} 
+                    onClick={(e) => getselected((child as any).props.children)}>
+                        { child }
+                    </div>
+                ))
+            }
         </div>
     )
 }
