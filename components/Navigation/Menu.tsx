2a6986b4415cccc93d95a0b0980173549016efd4
vorxsaken <jhonyevans6326@gmail.com>
2023-07-29 12:39:27 +0700

    chore: add categories and change menu options

--- components/Navigation/Menu.tsx
+++ components/Navigation/Menu.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import Link from 'next/link'
+import Link from "next/link"
+import Categories from "./Categories"
+
 
 export default function Menu() {
     return (
         <div className="flex justify-center items-center gap-6 font-semibold text-slate-600 text-[0.9rem]">
             <Link href={'/'} className="hover:text-red-500 transition-all duration-300 ease-in-out cursor-pointer">Home</Link>
-            <div className="hover:text-red-500 transition-all duration-300 ease-in-out cursor-pointer">About</div>
-            <div className="hover:text-red-500 transition-all duration-300 ease-in-out cursor-pointer">Recipes</div>
-            <div className="hover:text-red-500 transition-all duration-300 ease-in-out cursor-pointer">Contact</div>
+            <Categories />
+            <Link href={'/about'} className="hover:text-red-500 transition-all duration-300 ease-in-out cursor-pointer">About</Link>
         </div>
     )
 }
