46d3e65bfcd853a7860498e41658a7a9310d3a1b
vorxsaken <jhonyevans6326@gmail.com>
2023-07-31 21:45:11 +0700

    chore: add props for dynamic title and image

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -1,14 +1,13 @@
-import Image from 'next/image'
-import Food from '../assets/images/food.png';
+import Image, { StaticImageData } from 'next/image'
 
-export default function CategoryCard() {
+export default function CategoryCard({title, image}: {title: string, image: string | StaticImageData}) {
   return (
-    <div className='w-48 h-48 flex flex-col gap-8 justify-center items-center flex-none'>
-        <div className='w-full h-20 flex justify-center items-center'>
-            <Image src={Food} alt='category' width={130} className='pointer-events-none' />
+    <div className='w-48 h-48 flex flex-col gap-2 justify-center items-center md:flex-none'>
+        <div className='w-36 h-36 flex justify-center items-center overflow-hidden relative rounded-xl'>
+            <Image src={image} alt='category' fill className='pointer-events-none object-cover' />
         </div>
         <div className='w-full text-center font-semibold text-slate-800 text-sm'>
-            Dish
+            {title}
         </div>
     </div>
   )
