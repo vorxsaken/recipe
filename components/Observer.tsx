7b4be2c03b3a2c9a6745d5b358a5d03e11663909
vorxsaken <jhonyevans6326@gmail.com>
2023-11-04 03:48:57 -0400

    fix: failed to fetch when observer appear

--- components/Observer.tsx
+++ components/Observer.tsx
@@ -1,28 +1,20 @@
 import Lottie from 'lottie-react';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import LoadingAnimation from '../assets/92025-loading.json'
 
-export default function Observer({trigger, small}: {trigger: () => void, small?: boolean}) {
-    const [isIntersecting, setIsIntersection] = useState(false);
+export default function Observer({ trigger, small }: { trigger: () => void, small?: boolean }) {
     const ref = useRef(null);
 
     useEffect(() => {
         const observer = new IntersectionObserver(
             ([entry]) => {
-                setIsIntersection(entry.isIntersecting);
+                if (entry.isIntersecting) {
+                    trigger();
+                }
             }
         );
         observer.observe(ref.current as any)
-
-        return () => observer.disconnect();
-
-    }, [isIntersecting])
-
-    useEffect(() => {
-        if(isIntersecting){
-            trigger()
-        }
-    }, [isIntersecting])
+    }, [])
 
     return (
         <div ref={ref} className="w-full flex justify-center items-center mt-6">
