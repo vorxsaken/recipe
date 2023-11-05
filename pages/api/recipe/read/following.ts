88494754d8e3c7b138a3e5289227aeb2bdd4308f
vorxsaken <jhonyevans6326@gmail.com>
2023-11-05 13:53:18 -0500

    chore: remove comments

--- pages/api/recipe/read/following.ts
+++ pages/api/recipe/read/following.ts
@@ -5,55 +5,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { id, skip } = JSON.parse(req.body);
 
     try {
-        // const followingRecipe = await database.user.findMany({
-        //     where: {
-        //         id
-        //     },
-        //     include: {
-        //         following: {
-        //             skip: skip,
-        //             take: 20,
-        //             select: {
-        //                 userFollow: {
-        //                     select: {
-        //                         user: {
-        //                             select: {
-        //                                 recipes: {
-        //                                     include: {
-        //                                         collections: {
-        //                                             select: {
-        //                                                 id: true,
-        //                                                 name: true
-        //                                             }
-        //                                         },
-        //                                         ratings: {
-        //                                             select: {
-        //                                                 id: true,
-        //                                                 value: true
-        //                                             }
-        //                                         }
-        //                                     },
-        //                                     orderBy: {
-        //                                         created_at: "asc"
-        //                                     }
-        //                                 }
-        //                             }
-        //                         }
-        //                     }
-        //                 }
-        //             }
-        //         }
-        //     },
-        // }).catch(error => { throw new Error(error) })
-        // const arrays: any[] = followingRecipe[0]?.following.map((foll: any) => foll.userFollow?.user.recipes) || [];
-        // let result: any[] = [];
-        // for(let i = 0; i < arrays?.length; i++) {
-        //     result = [...result, ...arrays[i]]
-        // }
-
-        // res.status(200).send(result);
-
-        const recipes = await prisma?.user.findUnique({
+        const recipes = await database?.user.findUnique({
             where: { id },
             select: {
                 following: {
